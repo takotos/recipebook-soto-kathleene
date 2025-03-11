bb7cfaa1d8fa557e42e68928a4731b2ae8e20556
Kathleene Soto <kat.csoto@gmail.com>
2025-03-12 00:01:59 +0800

    Lab 1

---
+++ recipebook/ledger/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from .views import recipe_list, recipe_1, recipe_2
+
+urlpatterns = [
+    path('recipes/list/', recipe_list, name='recipe_list'),
+    path('recipe/1/', recipe_1, name='recipe_1'),
+    path('recipe/2/', recipe_2, name='recipe_2'),
+]
+
+app_name = "ledger"
